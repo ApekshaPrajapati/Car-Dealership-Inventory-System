f4a635d04dd4fc836827c85463848bed257b0b27
ApekshaPrajapati <apeksha120906@gmail.com>
2026-07-22 21:14:55 +0530

    modified and deploy backend on render

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/car_dealership';
-
+console.log(process.env.MONGO_URI);
 mongoose
   .connect(MONGO_URI)
   .then(() => {
@@ -15,3 +15,10 @@ mongoose
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
   });
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://car-dealership-inventory-system-1nlt.onrender.com'
+  ],
+  credentials: true
+}))
